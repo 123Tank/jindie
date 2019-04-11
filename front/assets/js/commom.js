c98bd3f73291046bb91b5a43698b7e702972494c
e50jaywe <976870729@qq.com>
2019-04-11 21:07:17 +0800

    调整文件结构，拆分公共头部和公共文件加载方式

---
+++ front/assets/js/commom.js
@@ -0,0 +1,29 @@
+document.write('<script src="assets/js/public/jquery.min.js""  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/bootstrap/bootstrap.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/jquery.flexslider-min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/owl.carousel.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/isotope.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/jquery.magnific-popup.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/jquery.scrollTo.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/smooth.scroll.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/jquery.appear.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/jquery.countTo.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/jquery.scrolly.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/plugins-scroll.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/imagesloaded.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/pace.min.js"  type="text/javascript"></script>');
+
+document.write('<script src="assets/js/public/main.js"  type="text/javascript"></script>');
---
+++ front/assets/js/commomCss.js
@@ -0,0 +1,16 @@
+
+document.write('<link href="assets/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/bootstrap/bootstrap-theme.min.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/style.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="http://www.jq22.com/jquery/font-awesome.4.6.0.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/ionicons.min.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/flexslider.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/owl.carousel.css" rel="stylesheet" type="text/css">');
+
+document.write('<link href="assets/css/magnific-popup.css" rel="stylesheet" type="text/css">');
